3baeed98f6bed6e63d31f390302694cf12d293cf
llpingll <94099281+llpingll@users.noreply.github.com>
2023-04-04 22:00:43 +0100

    Add funcionality to open and close modal

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+// Get variables
+const firstName = document.querySelector("#firstName");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const checkbox = document.querySelector("#checkbox");
+const submit = document.querySelector("#submit");
+const modal = document.querySelector(".modalContainer");
+const addBook = document.querySelector(".addBook");
+
+// SCRIPT
+// List of books
 let myLibrary = [];
 
+// Book constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,10 +19,24 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+// Book prototype
 Book.prototype.info = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 }
 
+// Events
+addBook.addEventListener("click", () => {
+    modal.classList.add("show");
+});
+
+modal.addEventListener("click", (e) => {
+    if (e.target == modal) {
+        modal.classList.remove("show");
+    }
+});
+
+
+// Function definitions
 function addBookToLibrary(book) {
     
 }
